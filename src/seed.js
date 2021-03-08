f2a1c7981588c82ec3cf00a96ffaecad8df0de8b
Joaquin Bordes Monroig <bordesmonroigj@gmail.com>
2021-03-08 14:18:43 -0500

    Change .png images by .jpg

--- src/seed.js
+++ src/seed.js
@@ -256,7 +256,7 @@ async function addDinosaurs() {
       name: 'Tyrannosaurus',
       description:
         'As long as a bus and twice the weight of an elephant, Tyrannosaurus was undoubtedly the top predator in its environment. Deep holes in the bones of prey such as Triceratops and Edmontosaurus show that Tyrannosaurus used its immensely powerful jaws and bone-piercing teeth as its main weapons. Small victims were probably shaken apart; larger animals were crippled by horrible injuries. Holding the body down with a foot, Tyrannosaurus used its huge neck muscles to tear off mouthfuls of flesh and bone with its mouth, before swallowing it all.',
-      image: `${baseUrl}/api/dinosaur/image/1.png`,
+      image: `${baseUrl}/api/dinosaur/image/1.jpg`,
       diet: 2,
       size: '39 ft (12 m)',
       locations: [1, 2],
@@ -269,7 +269,7 @@ async function addDinosaurs() {
       name: 'Triceratops',
       description:
         'Triceratops’s neck was probably quite flexible, helping it to feed not only on tree leaves but also on low-growing plants. Its powerful parrotlike beak helped it pluck tough forest vegetation, such as palms, ferns, and cycads. Its teeth were like scissors—shredding and snipping the plants.',
-      image: `${baseUrl}/api/dinosaur/image/2.png`,
+      image: `${baseUrl}/api/dinosaur/image/2.jpg`,
       diet: 1,
       size: '30 ft (9 m)',
       locations: [1, 2],
@@ -282,7 +282,7 @@ async function addDinosaurs() {
       name: 'Stegosaurus',
       description:
         'Large, diamond-shaped plates ran along the back of this famous dinosaur. Although the plates would have made Stegosaurus look bigger and more fearsome, they were no good as armor. It’s more likely they evolved for use in social or courtship displays. Stegosaurus had a toothless beak made of a hornlike substance. At the back of its mouth were rows of teeth that it used to crush leaves, chewing them to a pulp with simple up-and-down movements.',
-      image: `${baseUrl}/api/dinosaur/image/3.png`,
+      image: `${baseUrl}/api/dinosaur/image/3.jpg`,
       diet: 1,
       size: '30 ft (9 m)',
       locations: [1, 3],
@@ -295,7 +295,7 @@ async function addDinosaurs() {
       name: 'Ankylosaurus',
       description:
         'Ankylosaurus was the largest ankylosaur ever. Hundreds of armor plates studded its thick skin, and small armor plates even covered its eyelids. The armor formed from bony plates called osteoderms that grew within the skin, much like the armor plating of a crocodile’s skin. Ankylosaurus was also equipped with a huge tail club that it could swing at attackers with bone-shattering force.',
-      image: `${baseUrl}/api/dinosaur/image/4.png`,
+      image: `${baseUrl}/api/dinosaur/image/4.jpg`,
       diet: 1,
       size: '20 ft (6 m)',
       locations: [1, 2],
@@ -308,7 +308,7 @@ async function addDinosaurs() {
       name: 'Brachiosaurus',
       description:
         'One of the largest sauropods, Brachiosaurus weighed an incredible 33–55 tons (30–50 metric tons)—nearly 12 times more than an African elephant. Brachiosaurus’s long neck helped it to feed at heights of more than 50 ft (15 m), which is twice as high as any giraffe can reach.',
-      image: `${baseUrl}/api/dinosaur/image/5.png`,
+      image: `${baseUrl}/api/dinosaur/image/5.jpg`,
       diet: 1,
       size: '75 ft (23 m)',
       locations: [1],
@@ -321,7 +321,7 @@ async function addDinosaurs() {
       name: 'Velociraptor',
       description:
         'Velociraptor played a starring role in Jurassic Park, where it was shown as twice its actual size. In reality it was a slender, feathered animal about the size of a wolf. The most spectacular fossil of Velociraptor is a complete skeleton locked in combat with a Protoceratops. Velociraptor had huge, flickable toe claws and long, clawed arms that unfolded like wings to grapple prey. Although no feathered fossils of Velociraptor have been found, its arm bones have quill nodes—small bumps to which long feathers were anchored.',
-      image: `${baseUrl}/api/dinosaur/image/6.png`,
+      image: `${baseUrl}/api/dinosaur/image/6.jpg`,
       diet: 2,
       size: '61⁄2 ft (2 m)',
       locations: [4],
@@ -334,7 +334,7 @@ async function addDinosaurs() {
       name: 'Baryonyx',
       description:
         'Remains of partly digested dinosaurs were found in Baryonyx’s fossilized stomach, indicating that it ate land animals as well as fish. It had a very long, low skull, and its jaws had 96 pointed teeth—twice as many as other members of its family. Baryonyx may have had a ridge on its back and a small crest on its snout. Baryonyx means “heavy claw,” referring to its huge, hooklike thumb claws, which it may have used to spear fish, as grizzly bears do today.',
-      image: `${baseUrl}/api/dinosaur/image/7.png`,
+      image: `${baseUrl}/api/dinosaur/image/7.jpg`,
       diet: 2,
       size: '30 ft (9 m)',
       locations: [3, 5, 6],
@@ -347,7 +347,7 @@ async function addDinosaurs() {
       name: 'Suchomimus',
       description:
         'Suchomimus, meaning “crocodile mimic,” got its name from its crocodile-like snout and sharp teeth, which it used to catch fish and other slippery prey. Compared to other meat eaters, it had long and powerful arms—perhaps it used them to reach into the water to grasp prey. A bladelike sail ran along its back and perhaps its tail. Suchomimus had more than 100 teeth along its jaw that slanted backward and were pointed like the prongs of a rake. Another set of longer teeth lay clustered at the tip of its snout.',
-      image: `${baseUrl}/api/dinosaur/image/8.png`,
+      image: `${baseUrl}/api/dinosaur/image/8.jpg`,
       diet: 2,
       size: '30 ft (9 m)',
       locations: [7],
@@ -360,7 +360,7 @@ async function addDinosaurs() {
       name: 'Pachycephalosaurus',
       description:
         'By comparing Pachycephalosaurus’s few fossils with those of its relatives, scientists figure that this dinosaur was about as long as a station wagon. It probably had a bulky body but the long, slender hind legs of a fast runner. Its small teeth suggest a diet of easily digested plants or, perhaps, a mixture of plants and animal foods such as eggs.',
-      image: `${baseUrl}/api/dinosaur/image/9.png`,
+      image: `${baseUrl}/api/dinosaur/image/9.jpg`,
       diet: 1,
       size: '15 ft (4.5 m)',
       locations: [1, 2],
@@ -373,7 +373,7 @@ async function addDinosaurs() {
       name: 'Parasaurolophus',
       description:
         'This creature’s head had a long crest containing hollow tubes. Perhaps Parasaurolophus tooted air out of the crest to make trumpetlike sounds to communicate with herd members. Its heavy, muscular build and wide shoulders may have helped it push through dense undergrowth in woodlands.',
-      image: `${baseUrl}/api/dinosaur/image/10.png`,
+      image: `${baseUrl}/api/dinosaur/image/10.jpg`,
       diet: 1,
       size: '30 ft (9 m)',
       locations: [1, 2],
@@ -386,7 +386,7 @@ async function addDinosaurs() {
       name: 'Edmontosaurus',
       description:
         'Edmontosaurus is named after Edmonton town in Alberta, Canada, where the first fossils were found in 1917. One of the largest hadrosaurs, it weighed up to 41⁄2 tons (4 metric tons). Hollow areas around its nostrils may have contained inflatable sacs that Edmontosaurus could expand like balloons and perhaps use to make sounds.',
-      image: `${baseUrl}/api/dinosaur/image/11.png`,
+      image: `${baseUrl}/api/dinosaur/image/11.jpg`,
       diet: 1,
       size: '43 ft (13 m)',
       locations: [1, 2],
@@ -399,7 +399,7 @@ async function addDinosaurs() {
       name: 'Gallimimus',
       description:
         'One of the best known of all ornithomimids is Gallimimus (“chicken mimic”). It was the largest ornithomimid, three times as tall as a man and, at 1,000 lb (450 kg) in weight, a lot heavier than any chicken. Gallimimus was the fastest sprinter of any dinosaur and could have outrun a racehorse. It had a birdlike skull, with a brain about the size of a golf ball (only slightly larger than an ostrich’s). Its long, toothless beak was used to pick up leaves, seeds, insects, and small mammals. Gallimimus had wide eye sockets with eyes facing sideways. This helped it spot enemies in almost any direction. Inside each eyeball was a supporting ring of small bony plates. Modern birds still have this feature.',
-      image: `${baseUrl}/api/dinosaur/image/12.png`,
+      image: `${baseUrl}/api/dinosaur/image/12.jpg`,
       diet: 3,
       size: '20 ft (6 m)',
       locations: [4],
